c22240a3e861b0e5742a40640c1b5f071a25dc92
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-10-10 08:28:28 +0200

    Ajput du MachingGame

---
+++ MatchingGame_VictorPholien/MatchingGame_VictorPholien/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace MatchingGame_VictorPholien
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random nbAlea = new Random(); // Ajout de la déclaration et de l'initialisation de Random
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐈","🐈",
+                "🐷","🐷",
+                "🐐","🐐",
+                "🦊","🦊",
+                "🐴","🐴",
+                "🦨","🦨",
+                "🦉","🦉",
+                "🐀","🐀",
+            };
+
+            foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+            {
+                int index = nbAlea.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+
+        TextBlock derniereTBClique; // on va l’utiliser pour faire une référence à la TextBlock sur laquelle on vient de cliquer
+        bool trouvePaire = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlockActif = sender as TextBlock;
+            if (!trouvePaire)
+            {
+                textBlockActif.Visibility = Visibility.Hidden;
+                derniereTBClique = textBlockActif;
+                trouvePaire = true;
+            }
+            else if (textBlockActif.Text == derniereTBClique.Text)
+            {
+                textBlockActif.Visibility = Visibility.Hidden;
+                trouvePaire = false;
+            }
+            else
+            {
+                derniereTBClique.Visibility = Visibility.Visible;
+                trouvePaire = false;
+            }
+        }
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int tempsEcoule = 0;
+        int nbPairesTrouvees = 0;
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tempsEcoule++;
+            txtTemps.Text = (tempsEcoule / 10F).ToString("0.0s");
+            if (nbPairesTrouvees == 8)
+            {
+                timer.Stop();
+                txtTemps.Text = txtTemps.Text + " - Rejouer ? ";
+                SetUpGame(); // Recommencer
+            }
+            else
+            {
+                tempsEcoule = 0;
+                nbPairesTrouvees = 0;
+                timer.Start();
+                else if (textBlockActif.Text == derniereTBClique.Text)
+                {
+                    nbPairesTrouvees++;
+                    textBlockActif.Visibility = Visibility.Hidden;
+                    trouvePaire = false;
+                }
+
+                foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+                {
+                    if (textBlock.Name != "txtTemps")
+                    {
+                        int index = nbAlea.Next(animalEmoji.Count);
+                        string nextEmoji = animalEmoji[index];
+                        textBlock.Text = nextEmoji;
+                        animalEmoji.RemoveAt(index);
+                    }
+                }
+            }
+        }
+
+        private void txtTemps_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (nbPairesTrouvees == 8)
+            {
+                SetUpGame();
+            }
+        }
+    }
+}
